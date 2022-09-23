be23cc2841dc07aad850dd6051147e1ecc8e7d4f
rituroy <ritu.roy@ucsf.edu>
2022-09-23 11:56:54 -0700

    removed unnecessary files

--- example/example_rmarkdown.R
+++
@@ -1,6 +0,0 @@
-setwd("/Users/royr/UCSF/singleCell/Dab-seq/example")
-
-#rmarkdown::render("sample.Rmd", clean=TRUE, output_format="BiocStyle::html_document")
-
-#render("sample.Rmd", html_document())
-render("example.Rmd", html_document())
