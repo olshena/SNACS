af64c34709790e2414653d1b6fc77ebb0833e541
vekennedy <63078801+vekennedy@users.noreply.github.com>
2024-05-02 21:19:36 -0700

    Update runSNACS.R to run directly from h5 file

--- analysis/runSNACS.R
+++ analysis/runSNACS.R
@@ -21,16 +21,37 @@ packageVersion("SNACS")
 
 hashColors <- c("indianred2","green3","dodgerblue3","magenta3")
 dirInput <- "../data/data_sequenced/"
-fileInput <- dir(dirInput,pattern="csv")
 dirOutput <- "../data/"
+file_path <- "SNACS5.genotypes.hdf5" 
 
-for (fId in 1:length(fileInput)) {
-    if (substr(fileInput[fId],1,6)%in%paste0("SNACS",5:7)) dirDepth <- "../data/Total Depth and Alt Depth with all SNPs/" else dirDepth <- NULL
-    snacsObj <- createInitialSNACSobject(fileName=fileInput[fId],dirName=dirInput,hashColors=hashColors,dirDepth=dirDepth)
-    print(snacsObj)
-    save(snacsObj,file=paste0(dirOutput,"snacsObj_init_",snacsObj$exptName,".RData"))
-    rm(snacsObj)
-}
+barcodes <- h5read(file_path, "/CELL_BARCODES")
+variants <- h5read(file_path, "/VARIANTS")
+genotypes <- h5read(file_path, "/GT")
+hashes <- h5read(file_path, "/ABS/clr")
+hashnames <- h5read(file_path, "/AB_DESCRIPTIONS")
+totaldepth <- h5read(file_path, "/DP")
+altdepth <- h5read(file_path, "/AD")
+
+mutMat <- matrix(as.numeric(genotypes), nrow = nrow(genotypes), ncol = ncol(genotypes))
+rownames(mutMat) <- variants
+colnames(mutMat) <- barcodes
+mutMat[mutMat == 2] <- 1
+mutMat[mutMat == 3] <- NA
+
+colnames(hashes) <- barcodes
+rownames(hashes) <- hashnames
+hashMat <- hashes
+
+depthTotal <- matrix(as.numeric(totaldepth), nrow = nrow(totaldepth), ncol = ncol(totaldepth))
+rownames(depthTotal) <- variants
+colnames(depthTotal) <- barcodes
+
+depthAlt <- matrix(as.numeric(altdepth), nrow = nrow(altdepth), ncol = ncol(altdepth))
+rownames(depthAlt) <- variants
+colnames(depthAlt) <- barcodes
+
+snacsObj <- SNACSList(mut=mutMat,hashes=hashMat,exptName=exptName,,hashColors=hashColors,
+                      depthTotal=totaldepth,depthAlt=altdepth)
 
 ## ------------------------------
 ## Filter data
